cff2ccb2ebcf0c68cc345d6120421c6037b53dd5
Simina Urda <siminaurda@yahoo.com>
2022-11-29 16:00:38 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Urda_Simina_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/Tizen/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Urda_Simina_Lab7;
+
+class Program : MauiApplication
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	static void Main(string[] args)
+	{
+		var app = new Program();
+		app.Run(args);
+	}
+}
