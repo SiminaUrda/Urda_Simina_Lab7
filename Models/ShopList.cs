d78388cf05d00a6e971a170b29627b38f05db133
Simina Urda <siminaurda@yahoo.com>
2022-12-21 22:53:16 +0200

    Lab10

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace Urda_Simina_Lab7.Models
 {
@@ -15,5 +16,8 @@ namespace Urda_Simina_Lab7.Models
 
         public string Description { get; set; }
         public DateTime Date { get; set; }
+
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
     }
 }
